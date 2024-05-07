6b4164af5b1cceac3fc1b94baede2e3fba4dc51d
Nazmos27 <nazmos789@gmail.com>
2024-05-07 18:34:34 +0600

    static data

---
+++ module3/src/3_7.ts
@@ -0,0 +1,28 @@
+{
+    // static
+
+    //Shared Data: Static properties are shared among all instances of a class. This means that changes to a static property will affect all instances of the class.
+
+    class Counter {
+        static count : number = 0;
+        increament(){
+            return Counter.count = Counter.count + 1
+        }
+        decreament(){
+            return Counter.count = Counter.count - 1
+        }
+        static test(){
+            return console.log('testinng static method');
+        }
+    }
+
+    const instance1 = new Counter()
+    console.log(instance1.increament());
+
+    const instance2 = new Counter()
+    console.log(instance2.decreament());
+
+    //Utility Methods: Static methods can be used as utility functions that are related to the class but do not depend on specific instance data. These methods can be called directly on the class without the need to create an instance.
+
+    Counter.test() //here no instance need to create to call test()
+}
